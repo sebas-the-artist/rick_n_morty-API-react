233801495d69725dd3153cabccbd4fa4e2b39e5b
sebastian <manijustworkhere@gmail.com>
2025-10-19 12:49:42 -0400

    hellow

--- src/components/ExploreLanding.jsx
+++ src/components/ExploreLanding.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 const ExploreLanding = () => {
   return (
-    <section id="explore__landing">
+    <section id="explore">
       <div className="explore__landing--wrapper">
-        <h1 className="explore__landing--header">Explore the Multiverse</h1>
+        <div className="explore__landing">
+          <h1 className="explore__landing--header">Explore the Multiverse</h1>
+          <div className="search__bar--wrapper">
+            <input
+              type="text"
+              className="explore__search--bar"
+              placeholder="MR. Meeseeks"
+            />
+            <button className="explore__search--btn">Search</button>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -4,8 +4,9 @@ import ExploreLanding from "../components/ExploreLanding";
 const Explore = () => {
   return (
     <div className="explore">
-      <section id="explore">explore page coming soon!</section>
-      <ExploreLanding />
+      <section id="explore">
+        <ExploreLanding />
+      </section>
     </div>
   );
 };
